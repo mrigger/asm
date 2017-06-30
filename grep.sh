9bfffd619d41b611d2a2284102f93aaa2eca7cf4
Manuel Rigger <manuel.rigger@jku.at>
2017-06-30 11:26:07 +0200

    Extend grep exclude filter

--- grep.sh
+++ grep.sh
@@ -1,4 +1,4 @@
-EXCLUDE="--exclude=*.vcxproj.filters --exclude=*.html --exclude=*.js --exclude=*.md"
+EXCLUDE="--exclude=*.vcxproj.filters --exclude=*.html --exclude=*.js --exclude=*.md --exclude=*.png --exclude=*.base64"
 
 find $1 -iname "*.s"
 grep -R "asm" $1 ${EXCLUDE}
